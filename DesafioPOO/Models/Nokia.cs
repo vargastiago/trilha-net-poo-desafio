5b7146bbdd1db4f718a09fe3071d0605d18b94c5
Tiago Vargas <tiagovargas@ymail.com>
2025-11-25 15:34:36 -0300

    Remove comentários TODO

--- DesafioPOO/Models/Nokia.cs
+++ DesafioPOO/Models/Nokia.cs
@@ -1,6 +1,5 @@
 namespace DesafioPOO.Models;
 
-// TODO: Herdar da classe "Smartphone"
 public class Nokia : Smartphone
 {
     public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
