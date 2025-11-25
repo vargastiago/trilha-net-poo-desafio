dca8a50c9822b17d8018bbd3ffca98c11394dfb9
Tiago Vargas <tiagovargas@ymail.com>
2025-11-25 15:31:17 -0300

    Adiciona desafio Trilha .NET POO

---
+++ DesafioPOO/Models/Smartphone.cs
@@ -0,0 +1,29 @@
+namespace DesafioPOO.Models;
+
+public abstract class Smartphone
+{
+    public string Numero { get; set; }
+    public string Modelo { get; }
+    public string Imei { get; }
+    public int Memoria { get; }
+
+    public Smartphone(string numero, string modelo, string imei, int memoria)
+    {
+        Numero = numero;
+        Modelo = modelo;
+        Imei = imei;
+        Memoria = memoria;
+    }
+
+    public void Ligar()
+    {
+        Console.WriteLine("Ligando...");
+    }
+
+    public void ReceberLigacao()
+    {
+        Console.WriteLine("Recebendo ligação...");
+    }
+
+    public abstract void InstalarAplicativo(string nomeApp);
+}
---
+++ DesafioPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "111111111", memoria: 64);
+
+nokia.Numero = "987654";
+Console.WriteLine($"{nokia.Numero} {nokia.Modelo} {nokia.Memoria}");
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone iphone = new Iphone(numero: "555333", modelo: "Modelo 2", imei: "222222222", memoria: 128);
+Console.WriteLine($"{iphone.Numero} {iphone.Modelo} {iphone.Memoria}");
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
